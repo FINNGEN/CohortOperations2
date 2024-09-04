c7604a432a04958c4a18f2bbb205d018b2f9ea1d
javier-gracia-tabuenca-tuni <javier.graciatabuenca@tuni.fi>
2024-09-04 10:04:48 +0300

    remove unnecesary

--- R/fct_Configuration.R
+++ R/fct_Configuration.R
@@ -1,68 +1,6 @@
 
 
 
-
-
-fct_databasesConfigToDatabasesHandlers <- function(
-    databasesConfig,
-    loadConnectionChecksLevel = "basicChecks") {
-
-  fct_assertdatabasesConfig(databasesConfig)
-
-  databasesHandlers <- list()
-  for(databaseId in names(databasesConfig)){
-
-    cohortTableHandlerConfig <- databasesConfig[[databaseId]]$cohortTableHandler
-
-    cohortTableHandler <- HadesExtras::createCohortTableHandlerFromList(cohortTableHandlerConfig, loadConnectionChecksLevel)
-
-    # TEMP
-    if( databaseId == 'E1'){
-      testCohortTable <-  tibble::tribble(
-        ~cohort_definition_id, ~subject_id, ~cohort_start_date, ~cohort_end_date,
-        1, 1, as.Date("2000-01-01"), as.Date("2000-12-01"),
-        1, 2, as.Date("2000-01-01"), as.Date("2000-12-01"),
-        1, 3, as.Date("2000-01-01"), as.Date("2000-12-01"),
-        1, 9, as.Date("2000-01-01"), as.Date("2000-12-01"),
-        1, 5, as.Date("2000-01-01"), as.Date("2000-12-01"),
-        1, 5, as.Date("2004-01-01"), as.Date("2004-12-01"),
-
-        2, 2, as.Date("2001-01-01"), as.Date("2002-12-01"),# non overlapping
-        2, 3, as.Date("2000-06-01"), as.Date("2000-09-01"),# inside
-        2, 9, as.Date("2000-06-01"), as.Date("2010-12-01"),# overlap
-        2, 5, as.Date("2004-06-01"), as.Date("2010-12-01"),# overlap with second
-        2, 6, as.Date("2000-01-01"), as.Date("2010-12-01")
-      )
-
-      testCohortDefinitionTable <- tibble::tribble(
-        ~cohort_definition_id, ~cohort_definition_name, ~cohort_definition_description, ~definition_type_concept_id, ~cohort_definition_syntax, ~subject_concept_id, ~cohort_initiation_date,
-        1, 'Diabetes Cohort', 'Cohort of patients diagnosed with diabetes', 1234, 'SELECT * FROM patients WHERE diagnosis = "Diabetes"', 5678, as.Date('2022-01-01'),
-        2, 'Hypertension Cohort', 'Cohort of patients diagnosed with hypertension', 1234, 'SELECT * FROM patients WHERE diagnosis = "Hypertension"', 5678, as.Date('2022-01-01'),
-        3, 'Obesity Cohort', 'Cohort of patients diagnosed with obesity', 1234, 'SELECT * FROM patients WHERE diagnosis = "Obesity"', 5678, as.Date('2022-01-01')
-      )
-
-      DatabaseConnector::insertTable(
-        connection = cohortTableHandler$connectionHandler$getConnection(),
-        table = "cohort",
-        data = testCohortTable
-      )
-
-      DatabaseConnector::insertTable(
-        connection = cohortTableHandler$connectionHandler$getConnection(),
-        table = "cohort_definition",
-        data = testCohortDefinitionTable
-      )
-    }
-
-    databasesHandlers[[databaseId]] <- list(cohortTableHandler = cohortTableHandler)
-  }
-
-  return(databasesHandlers)
-
-}
-
-
-
 fct_checkdatabasesConfig  <- function(databasesConfig) {
   collection <- .fct_assertdatabasesConfig(databasesConfig)
   if (collection$isEmpty()) {
