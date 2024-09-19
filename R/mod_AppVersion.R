343c589bc4f9a864e8cf49fef91559c5c0197f87
javier-gracia-tabuenca-tuni <javier.graciatabuenca@tuni.fi>
2024-09-19 13:56:13 +0300

    added appVersion mod and manual tesr

---
+++ R/mod_AppVersion.R
@@ -0,0 +1,79 @@
+#' @title mod_appVersion_ui
+#'
+#' @description UI module for selecting and connecting to databases.
+#'
+#' @param id Shiny module ID.
+#'
+#' @return Shiny UI elements for the database selection module.
+mod_appVersion_ui <- function(id) {
+  ns <- shiny::NS(id)
+  shiny::tagList(
+    shiny::h4("System version:"),
+    shiny::htmlOutput(ns("systemVersion_textOutput")),
+    shiny::br(),
+    shiny::h4("App and AnalysisModules version:"),
+    reactable::reactableOutput(ns("appVersion_reactable")),
+    shiny::br(),
+    shiny::h4("Dependencies version:"),
+    reactable::reactableOutput(ns("dependencies_reactable"))
+  )
+}
+
+#' @title mod_appVersion_server
+#'
+#' @description Server module for handling database selection and connection.
+#'
+#' @param id Shiny module ID.
+#' @param databasesConfig Configuration list for databases.
+#' @param r_databaseConnection Reactive values for database connection.
+#'
+#' @return Shiny server logic for the database selection module.
+mod_appVersion_server <- function(id, databasesConfig, r_databaseConnection) {
+  shiny::moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+
+    rVersion  <- R.Version()
+    dependenciesList <- renv::lockfile_read()$Packages
+    # convert to a tibble
+    dependenciesTibble <- tibble::tibble()
+    for (package in names(dependenciesList)){
+      dependenciesTibble <- dplyr::bind_rows(
+        dependenciesTibble,
+        tibble::tibble(
+        Package = dependenciesList[[package]]$Package,
+        Version = dependenciesList[[package]]$Version,
+        Source = dependenciesList[[package]]$Source,
+        Repository = dependenciesList[[package]]$Repository,
+        Hash = dependenciesList[[package]]$Hash
+        )
+      )
+    }
+
+    output$systemVersion_textOutput <- shiny::renderUI({
+      paste0("System version: ", rVersion$platform, "<br>",
+             "R version: ", rVersion$version.string)  |>
+        htmltools::HTML()
+    })
+
+    output$appVersion_reactable <- reactable::renderReactable({
+      appVersion <- dependenciesTibble  |>
+        # TEMP hard code CO2AnalysisModules
+        dplyr::filter(Package %in% c("CohortOperations2", "CO2AnalysisModules"))
+
+      appVersion |> reactable::reactable(
+        filterable = FALSE,
+        pagination = FALSE
+      )
+    })
+
+    output$dependencies_reactable <- reactable::renderReactable({
+      dependenciesTibble |>
+      dplyr::arrange(Package) |>
+      reactable::reactable(
+        filterable = TRUE,
+        pagination = TRUE
+      )
+    })
+  })
+}
+
--- tests/testmanual/manualtest-mod_AppVersion.R
+++ tests/testmanual/manualtest-mod_AppVersion.R
@@ -3,16 +3,25 @@ devtools::load_all(".")
 source(testthat::test_path("setup.R"))
 source(testthat::test_path("helper.R"))
 
+fcr_setUpLogger()
+
+r_databaseConnection <- shiny::reactiveValues(
+  cohortTableHandler = NULL,
+  atlasConfig = NULL,
+  hasChangeCounter = 0
+)
+
+databasesConfig <- test_databasesConfig
 
 # run module --------------------------------------------------------------
 devtools::load_all(".")
 
 app <- shiny::shinyApp(
   shiny::fluidPage(
-    mod_appVersion_ui("select_configuration")
+    mod_selectDatabases_ui("select_configuration")
   ),
   function(input,output,session){
-    mod_appVersion_server("select_configuration")
+    mod_selectDatabases_server("select_configuration", databasesConfig, r_databaseConnection)
   },
   options = list(launch.browser=TRUE)
 )
