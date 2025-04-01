0b14e87c53ac4a6272834d76677c50c4e987dea4
javier-gracia-tabuenca-tuni <javier.graciatabuenca@tuni.fi>
2025-04-01 11:58:33 +0300

    dd

--- R/mod_ImportCohortsFromAtlas.R
+++ R/mod_ImportCohortsFromAtlas.R
@@ -185,7 +185,7 @@ mod_importCohortsFromAtlas_server <- function(id, r_databaseConnection, filterCo
 
           # get the timestamp of the latest generation
           cohortGenerationTimestamp <- .getCohortGenerationTimestamp(cohortId, webApiUrl, sourceKey)
-          print(cohortGenerationTimestamp)
+          ParallelLogger::logInfo(cohortGenerationTimestamp)
           # cohortStatus
           cohortStatus <- "NOT GENERATED"
           if (!is.null(cohortGenerationTimestamp)) {
