4e67168daf69f7cb66587acb09cafab203aebbad
Yohannes <dyohanne@ad.helsinki.fi>
2025-04-22 17:20:08 +0300

    Fix: Modal dialog shows task relevant log entries. Upon shiny disconnect, show log entries.

--- R/fct_Configuration.R
+++ R/fct_Configuration.R
@@ -50,59 +50,6 @@ fct_assertdatabasesConfig  <- function(databasesConfig) {
   return(collection)
 }
 
-#' Set Up Logger
-#'
-#' Sets up a logger with console and file appenders for logging.
-#'
-#' @return A logger object.
-#'
-#' @export
-fcr_setUpLogger  <- function(){
-
-  timestamp  <- timestamp <- as.character(as.numeric(format(Sys.time(), "%d%m%Y%H%M%OS2"))*100)
-  folderWithLog <- file.path(tempdir(),  paste0("logs", timestamp))
-  dir.create(folderWithLog, showWarnings = FALSE)
-  logger <- ParallelLogger::createLogger(
-    threshold = "TRACE",
-    appenders = list(
-      # to console for tracking
-      .createConsoleAppenderForSandboxLogging(),
-      # to file for showing in app
-      ParallelLogger::createFileAppender(
-        fileName = file.path(folderWithLog, "log.txt"),
-        layout = ParallelLogger::layoutSimple
-      )
-    )
-  )
-  ParallelLogger::clearLoggers()
-  #addDefaultFileLogger(file.path(folderWithLog, "log2.txt"))
-  ParallelLogger::registerLogger(logger)
-
-  shiny::addResourcePath("logs", folderWithLog)
-
-}
-
-
-#' Create Console Appender for Sandbox Logging
-#'
-#' Creates a console appender for sandbox logging with a specified layout.
-#'
-#' @param layout A layout function for the logger. Defaults to ParallelLogger::layoutParallel.
-#'
-#' @return An appender object for logging.
-#'
-.createConsoleAppenderForSandboxLogging <- function(layout = ParallelLogger::layoutParallel) {
-  appendFunction <- function(this, level, message, echoToConsole) {
-    # Avoid note in check:
-    missing(this)
-    message <- paste0("[sandbox-co2-log] ", message)
-    writeLines(message, con = stderr())
-  }
-  appender <- list(appendFunction = appendFunction, layout = layout)
-  class(appender) <- "Appender"
-  return(appender)
-}
-
 #' Convert String to Function
 #'
 #' Converts a string in the format "package::function" to the actual function.
