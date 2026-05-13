3419d4458d0e8ce2e8c7a0fb4ad967ca556e70fa
DawitYohannes <dyohanne@ad.helsinki.fi>
2026-05-14 00:57:53 +0300

    Use faster workbench table renderer

--- R/mod_CohortWorkbench.R
+++ R/mod_CohortWorkbench.R
@@ -244,7 +244,7 @@ mod_cohortWorkbench_server <- function(id, r_databaseConnection, r_workbenchCach
       }
 
       get_cohorts_summary() |>
-        HadesExtras::rectable_cohortsSummary(
+        .rectable_cohortsSummary_fast(
           deleteButtonsShinyId = ns("cohortsWorkbenchDeleteButtons"),
           editButtonsShinyId = ns("cohortsWorkbenchEditButtons"))
     })
@@ -594,4 +594,303 @@ mod_cohortWorkbench_server <- function(id, r_databaseConnection, r_workbenchCach
 
 
   })
+}
+
+
+.rectable_cohortsSummary_fast <- function(
+    cohortsSummary,
+    deleteButtonsShinyId = NULL,
+    editButtonsShinyId = NULL) {
+  cohortNameNcharLimit <- 15L
+
+  cohortsSummary |> HadesExtras::assertCohortsSummary()
+  deleteButtonsShinyId |> checkmate::assertString(null.ok = TRUE)
+  editButtonsShinyId |> checkmate::assertString(null.ok = TRUE)
+
+  cohortsSummaryToPlot <- cohortsSummary |>
+    dplyr::mutate(
+      fullName = cohortName,
+      database = paste0(
+        databaseId,
+        "<br>",
+        dplyr::if_else(
+          nchar(databaseName) > cohortNameNcharLimit,
+          paste0(substr(databaseName, 1, cohortNameNcharLimit), "..."),
+          databaseName
+        )
+      ),
+      cohort = paste0(
+        shortName,
+        "<br>",
+        dplyr::if_else(
+          nchar(fullName) > 35,
+          paste0(substr(fullName, 1, 35), "..."),
+          fullName
+        )
+      ),
+      databaseTooltip = paste0(
+        "Database Id: ", databaseId, "\n",
+        "Database Name: ", databaseName
+      ),
+      cohortTooltip = paste0(
+        "Short name: ", shortName, "\n",
+        "Full name: ", fullName
+      ),
+      cohortCountsStr = paste0(
+        cohortSubjects,
+        "<br>",
+        "(", cohortEntries, ")"
+      ),
+      cohortCountsStrTooltip = paste0(
+        "Cohort has ", cohortEntries, " entries\n",
+        "from ", cohortSubjects, " unique subjects."
+      ),
+      cohortStartYearRange = purrr::map_chr(histogramCohortStartYear, .histogram_year_range),
+      cohortEndYearRange = purrr::map_chr(histogramCohortEndYear, .histogram_year_range),
+      countSexStr = purrr::map_chr(sexCounts, .sexTibbleToStr_fast),
+      countSexStrTooltip = purrr::map_chr(sexCounts, .sexTibbleToTooltipStr_fast),
+      buildInfoStr = purrr::map_chr(buildInfo, .buildInfoStr_fast),
+      buildInfoTooltip = purrr::map_chr(buildInfo, .buildInfoToTooltipStr_fast),
+      deleteButton = NA,
+      editButton = NA
+    )
+
+  columns <- list(
+    database = reactable::colDef(
+      name = "Database",
+      cell = function(value, index) {
+        .titleText(value, cohortsSummaryToPlot$databaseTooltip[[index]])
+      },
+      html = TRUE,
+      maxWidth = 200
+    ),
+    cohort = reactable::colDef(
+      name = "Cohort",
+      cell = function(value, index) {
+        .titleText(value, cohortsSummaryToPlot$cohortTooltip[[index]])
+      },
+      html = TRUE
+    ),
+    cohortCountsStr = reactable::colDef(
+      name = "N Subjects <br> (N Entries)",
+      cell = function(value, index) {
+        .titleText(value, cohortsSummaryToPlot$cohortCountsStrTooltip[[index]])
+      },
+      html = TRUE,
+      maxWidth = 100
+    ),
+    cohortStartYearRange = reactable::colDef(
+      name = "Cohort Start Date",
+      cell = function(value) {
+        .titleText(value, value)
+      },
+      maxWidth = 150
+    ),
+    cohortEndYearRange = reactable::colDef(
+      name = "Cohort End Date",
+      cell = function(value) {
+        .titleText(value, value)
+      },
+      maxWidth = 150
+    ),
+    countSexStr = reactable::colDef(
+      name = "Sex",
+      style = function(value) {
+        .barStyle_fast(perSexStr = value)
+      },
+      cell = function(value, index) {
+        .titleText(value, cohortsSummaryToPlot$countSexStrTooltip[[index]])
+      },
+      align = "left",
+      maxWidth = 160
+    ),
+    buildInfoStr = reactable::colDef(
+      name = "Build Info",
+      cell = function(value, index) {
+        .titleText(value, cohortsSummaryToPlot$buildInfoTooltip[[index]])
+      },
+      align = "center",
+      maxWidth = 80
+    )
+  )
+
+  if (!is.null(deleteButtonsShinyId)) {
+    columns[["deleteButton"]] <- reactable::colDef(
+      name = "",
+      sortable = FALSE,
+      cell = function() htmltools::tags$button(shiny::icon("trash")),
+      maxWidth = 40
+    )
+  }
+
+  if (!is.null(editButtonsShinyId)) {
+    edit_col <- list(
+      editButton = reactable::colDef(
+        name = "",
+        sortable = FALSE,
+        cell = function() htmltools::tags$button(shiny::icon("edit")),
+        maxWidth = 40
+      )
+    )
+    columns <- append(columns, edit_col, after = match("cohort", names(columns)))
+  }
+
+  onClick <- ""
+  if (!is.null(deleteButtonsShinyId) | !is.null(editButtonsShinyId)) {
+    onClick <- paste0(
+      onClick,
+      "function(rowInfo, column) {
+          if (column.id == 'editButton') {
+            if (window.Shiny) {
+            Shiny.setInputValue('", editButtonsShinyId, "', { index: rowInfo.index + 1 }, { priority: 'event' })
+            }
+          }
+          if (column.id == 'deleteButton') {
+            if (window.Shiny) {
+            Shiny.setInputValue('", deleteButtonsShinyId, "', { index: rowInfo.index + 1 }, { priority: 'event' })
+            }
+          }
+          return false
+        }
+      "
+    )
+  }
+
+  cohortsSummaryToPlot |>
+    dplyr::select(names(columns)) |>
+    reactable::reactable(
+      columns = columns,
+      onClick = reactable::JS(onClick)
+    )
+}
+
+
+.titleText <- function(text, tooltip) {
+  htmltools::tags$span(
+    title = tooltip,
+    htmltools::HTML(text)
+  )
+}
+
+
+.histogram_year_range <- function(data) {
+  if (is.null(data) || nrow(data) == 0 || !all(c("year", "n") %in% names(data))) {
+    return("-")
+  }
+
+  years <- data$year[!is.na(data$year) & !is.na(data$n) & data$n > 0]
+  if (length(years) == 0) {
+    return("-")
+  }
+
+  if (min(years) == max(years)) {
+    return(as.character(min(years)))
+  }
+
+  paste0(min(years), "-", max(years))
+}
+
+
+.barStyle_fast <- function(
+    perSexStr,
+    colorSexMale = "#2c5e77",
+    colorSexFemale = "#BF616A",
+    colorSexNa = "#8C8C8C") {
+  values <- strsplit(perSexStr, "\\s+")[[1]]
+  values <- as.double(gsub("%", "", values))
+  if (length(values) < 3 || any(is.na(values[1:3]))) {
+    values <- c(0, 0, 0)
+  }
+
+  p_male <- values[1]
+  p_na <- values[2]
+
+  list(
+    backgroundImage = paste0(
+      "linear-gradient(to right, ",
+      colorSexMale, " ", p_male, "%, ",
+      colorSexNa, " ", p_male, "%, ", colorSexNa, " ", p_male + p_na, "%, ",
+      colorSexFemale, " ", p_male + p_na, "%, ",
+      colorSexFemale,
+      ")"
+    ),
+    backgroundSize = paste("100%", "75%"),
+    backgroundRepeat = "no-repeat",
+    backgroundPosition = "center",
+    color = "#FFFFFF"
+  )
+}
+
+
+.sexTibbleToStr_fast <- function(data) {
+  sex_counts <- .sex_counts(data)
+  n_total <- sum(sex_counts)
+  if (n_total == 0) {
+    return("0% 0% 0%")
+  }
+
+  p_male <- round(sex_counts[["male"]] / n_total * 10000) / 100
+  p_na <- round(sex_counts[["unknown"]] / n_total * 10000) / 100
+  p_female <- round(sex_counts[["female"]] / n_total * 10000) / 100
+
+  paste0(p_male, "% ", p_na, "% ", p_female, "%")
+}
+
+
+.sexTibbleToTooltipStr_fast <- function(data) {
+  sex_counts <- .sex_counts(data)
+  n_total <- sum(sex_counts)
+  if (n_total == 0) {
+    return("No sex counts available")
+  }
+
+  p_male <- round(sex_counts[["male"]] / n_total * 10000) / 100
+  p_na <- round(sex_counts[["unknown"]] / n_total * 10000) / 100
+  p_female <- round(sex_counts[["female"]] / n_total * 10000) / 100
+
+  paste0(
+    "Males: ", sex_counts[["male"]], " (", p_male, "%)\n",
+    "Unknown: ", sex_counts[["unknown"]], " (", p_na, "%)\n",
+    "Female: ", sex_counts[["female"]], " (", p_female, "%)"
+  )
+}
+
+
+.sex_counts <- function(data) {
+  if (is.null(data) || nrow(data) == 0 || !all(c("sex", "n") %in% names(data))) {
+    return(c(male = 0, unknown = 0, female = 0))
+  }
+
+  male <- sum(data$n[data$sex == "MALE"], na.rm = TRUE)
+  female <- sum(data$n[data$sex == "FEMALE"], na.rm = TRUE)
+  unknown <- sum(data$n[!(data$sex %in% c("MALE", "FEMALE"))], na.rm = TRUE)
+
+  c(male = male, unknown = unknown, female = female)
+}
+
+
+.buildInfoStr_fast <- function(buildInfo) {
+  logTibble <- buildInfo$logTibble
+  if (is.null(logTibble) || nrow(logTibble) == 0 || !"type" %in% names(logTibble)) {
+    return("")
+  }
+
+  logTibble |>
+    dplyr::count(type) |>
+    dplyr::transmute(str = paste0(type, "(", n, ")")) |>
+    dplyr::pull(str) |>
+    paste(collapse = ", ")
+}
+
+
+.buildInfoToTooltipStr_fast <- function(buildInfo) {
+  logTibble <- buildInfo$logTibble
+  if (is.null(logTibble) || nrow(logTibble) == 0 || !"message" %in% names(logTibble)) {
+    return("")
+  }
+
+  logTibble |>
+    dplyr::transmute(str = paste0(type, " ", message)) |>
+    dplyr::pull(str) |>
+    paste(collapse = "\n")
 }
